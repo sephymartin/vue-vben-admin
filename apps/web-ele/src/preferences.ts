c915e55a1cf4535f1b3e9d0be17f2a1bbe2d5531
sephymartin <sephymartin@gmail.com>
2025-11-17 23:48:02 +0800

    chore: 登录界面调整

--- apps/web-ele/src/preferences.ts
+++ apps/web-ele/src/preferences.ts
@@ -9,5 +9,9 @@ export const overridesPreferences = defineOverridesPreferences({
   // overrides
   app: {
     name: import.meta.env.VITE_APP_TITLE,
+    authPageLayout: 'panel-center',
+  },
+  theme: {
+    mode: 'light',
   },
 });
